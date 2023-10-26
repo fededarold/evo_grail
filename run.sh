ff4f356c7679a94ba0d6364c9f8de3a66a23ad66
fededarold <118625395+fededarold@users.noreply.github.com>
2023-10-26 05:53:05 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#export OMP_NUM_THREADS=1
+# export OMP_SCHEDULE=STATIC
+# export OMP_PROC_BIND=CLOSE
+python3 mp_test_grail_v4.py 
+# k=1
+# for i in {10..14}
+# do
+        # echo "conf $i"
+        # echo $( expr $i + $k )
+        # taskset -cp $i
+        # taskset -c $i python3 run_experiment_dqn_pong.py --conf $( expr $i + $k ) &
+# done
+# wait
