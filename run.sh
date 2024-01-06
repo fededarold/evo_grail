ab2e907c4377c37a52039bc23d4500e0fb8826c5
fededarold <118625395+fededarold@users.noreply.github.com>
2024-01-06 05:35:54 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 # python3 mp_test_grail_beta_v3.py 
 
-python3 run_experiment_population_2024.py --timestamp 1652780480 --individual_id 0 
-# python3 run_experiment_population_2024.py --timestamp 1652780480 --individual_id 1 &
-# python3 run_experiment_population_2024.py --timestamp 1652780480 --individual_id 2 &
+python3 run_experiment_population_2024.py --timestamp 1652780480 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652780480 --individual_id 1 &
+python3 run_experiment_population_2024.py --timestamp 1652780480 --individual_id 2 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652780481 --individual_id 0 &
-# python3 run_experiment_population_2024.py --timestamp 1652780481 --individual_id 1 &
-# python3 run_experiment_population_2024.py --timestamp 1652780481 --individual_id 2 &
+python3 run_experiment_population_2024.py --timestamp 1652780481 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652780481 --individual_id 1 &
+python3 run_experiment_population_2024.py --timestamp 1652780481 --individual_id 2 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 0 &
-# python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 1 &
-# python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 2 &
-# python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 3 &
-# python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 4 &
+python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 1 &
+python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 2 &
+python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 3 &
+python3 run_experiment_population_2024.py --timestamp 1652781481 --individual_id 4 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652780482 --individual_id 0 &
-# python3 run_experiment_population_2024.py --timestamp 1652780482 --individual_id 1 &
+python3 run_experiment_population_2024.py --timestamp 1652780482 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652780482 --individual_id 1 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652780483 --individual_id 0 &
-# python3 run_experiment_population_2024.py --timestamp 1652780483 --individual_id 1 &
+python3 run_experiment_population_2024.py --timestamp 1652780483 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652780483 --individual_id 1 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652781484 --individual_id 0 &
-# python3 run_experiment_population_2024.py --timestamp 1652781484 --individual_id 1 &
+python3 run_experiment_population_2024.py --timestamp 1652781484 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652781484 --individual_id 1 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 0 &
-# python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 1 &
-# python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 2 &
-# python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 3 &
-# python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 4 &
+python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 1 &
+python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 2 &
+python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 3 &
+python3 run_experiment_population_2024.py --timestamp 1652780485 --individual_id 4 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652780486 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652780486 --individual_id 0 &
 
-# python3 run_experiment_population_2024.py --timestamp 1652780487 --individual_id 0 &
+python3 run_experiment_population_2024.py --timestamp 1652780487 --individual_id 0 &
